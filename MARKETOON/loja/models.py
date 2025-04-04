040690d696845a010b38d4195175292f2d1d05bb
Davilapeixoto <davilapeixoto52@gmail.com>
2025-04-04 15:16:02 -0300

    ajustes

--- MARKETOON/loja/models.py
+++ MARKETOON/loja/models.py
@@ -5,6 +5,6 @@ class func_registrar_produto(models.Model):
     nome=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
     telefone=models.CharField(max_length=11)
-    preco=models.CharField(max_length=100)
+    preco=models.DecimalField(max_digits=10,decimal_places=2)
     descricao= models.TextField(blank=True,null=True)
     data_criacao=models.DateTimeField(auto_now=True,verbose_name="Data de criação")
