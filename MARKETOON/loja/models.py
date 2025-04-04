450d931ece60148d1e6b6e8186a208a4778c0cfe
Davilapeixoto <davilapeixoto52@gmail.com>
2025-04-04 14:58:45 -0300

    registro

--- MARKETOON/loja/models.py
+++ MARKETOON/loja/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class func_registrar_produto(models.Model):
+    nome=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    telefone=models.CharField(max_length=11)
+    preco=models.CharField(max_length=100)
+    descricao= models.TextField(blank=True,null=True)
+    data_criacao=models.DateTimeField(auto_now=True,verbose_name="Data de criação")
--- MARKETOON/loja/views.py
+++ MARKETOON/loja/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render,redirect
+from loja.models import func_registrar_produto
 
 # Create your views here.
+def cadastro_produto(request):
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        email = request.POST.get("email")
+        telefone = request.POST.get("telefone")
+        descricao = request.POST.get("descricao")
+        preco = request.POST.get("preco")
+        data=request.POST.get('data')
+        func_registrar_produto.objects.create(nome=nome,
+                                              email=email,
+                                              telefone=telefone,
+                                              descricao=descricao,
+                                              preco=preco,
+                                              data=data
+                                              )
