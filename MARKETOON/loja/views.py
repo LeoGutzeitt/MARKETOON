5f49e15fccef9efb39c23a53b6e6be29a1423313
Davilapeixoto <davilapeixoto52@gmail.com>
2025-04-06 15:39:06 -0300

    pesquisa funcional

--- MARKETOON/loja/views.py
+++ MARKETOON/loja/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Wishlist
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Q
 
 
 # Create your views here.
@@ -13,11 +14,20 @@ def detalhes_produto(request, id):
 
 
 def home(request):
-    lista_produtos = func_registrar_produto.objects.all()
-    paginator = Paginator(lista_produtos, 12)
-    pagina = request.GET.get('page')
-    produtos = paginator.get_page(pagina)
-    return render(request, 'loja/home.html',{'produtos': produtos})
+    query = request.GET.get('q', '')
+    produtos = func_registrar_produto.objects.all()
+
+    if query:
+        produtos = produtos.filter(
+            Q(nome__icontains=query) |
+            Q(descricao__icontains=query)
+        )
+
+    paginator = Paginator(produtos, 12)  # ou a paginação que estiver usando
+    page = request.GET.get('page')
+    produtos_paginados = paginator.get_page(page)
+
+    return render(request, 'loja/home.html', {'produtos': produtos_paginados})
 
 def cadastro_produto(request):
     if request.method == "POST":
