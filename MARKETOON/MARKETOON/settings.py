eae67bd3c470ae1c036d800cfccee2fb7bdb30ab
AlineTakakura <aline.mtakakura@gmail.com>
2025-04-26 19:10:53 -0300

    corrigi o erro relacionado ao CSRF

--- MARKETOON/MARKETOON/settings.py
+++ MARKETOON/MARKETOON/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", "chave-insegura-para-dev")
 DEBUG = os.getenv("DEBUG", "True").lower() in ["true", "1", "t"]
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1','http://localhost']
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
