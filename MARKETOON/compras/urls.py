0472c4f9bec29accb7aad403eedc19c43d0eb299
gabrielvictalino <gabrielvictalino@gmail.com>
2025-04-28 18:39:01 -0300

    voltei um commit

--- MARKETOON/compras/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.pagina_inicial, name='pagina_inicial'),
-    path('comprar/<int:produto_id>/', views.comprar_produto, name='comprar_produto'),
-]
