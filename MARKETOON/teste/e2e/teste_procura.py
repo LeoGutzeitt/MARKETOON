713244c63755f4fb28d793d7d909f67b6037528d
AlineTakakura <aline.mtakakura@gmail.com>
2025-04-29 16:24:32 -0300

    Alteração no teste da barra de procura

--- MARKETOON/teste/e2e/teste_procura.py
+++ MARKETOON/teste/e2e/teste_procura.py
@@ -1,4 +1,3 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,8 +9,6 @@ def test_buscar_produto():
 
     try:
     
-        time.sleep(10)
-        
         driver.get("http://127.0.0.1:8000/")  
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.NAME, "q"))
@@ -22,10 +19,10 @@ def test_buscar_produto():
         barra_busca.send_keys(Keys.RETURN)
 
         WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "produto-item"))  
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "product-card"))  
         )
         
-        produtos = driver.find_elements(By.CLASS_NAME, "produto-item")
+        produtos = driver.find_elements(By.CLASS_NAME, "product-card")
         for produto in produtos:
             assert "Aline" in produto.text, f"Produto inesperado: {produto.text}"
 
