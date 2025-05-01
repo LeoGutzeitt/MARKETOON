de5f2e4daf4df203f27a9726085413e2e998c55c
LeoGutzeitt <leonardogbmatos@hotmail.com>
2025-04-30 22:02:40 -0300

    ajeitando o js

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,19 @@
+describe('Como artista gostaria de poder lançar meus produtos na plataforma'), () => {
+    it('cadastrando produto', () => {
+        cy.visit('/');
+
+
+
+
+
+
+    })
+
+    it('cenario2', () => {
+        //steps do cenario2
+    })
+
+    it('cenario3', () => {
+        //steps do cenario3
+    })
+}
