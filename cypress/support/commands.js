1d9ffd6f5d8acb173529484647df1bd70a683fac
AlineTakakura <aline.mtakakura@gmail.com>
2025-05-30 20:00:51 -0300

    Alteração filtros

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('deletedatabase', () => {
+  cy.exec('python delete_database.py', { failOnNonZeroExit: false });
+});
